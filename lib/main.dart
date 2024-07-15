a22bef4f4ff2f0f8e6d1b1c5e770585950db4b22
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-07-15 18:12:30 +0700

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,118 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen()
+    );
+  }
+}
+
+// Contoh StatelessWidget
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({
+    super.key,
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello, world!'),
+    );
+  }
+}
+
+// Contoh StatefulWidget
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({
+    super.key,
+    required this.text
+  });
+
+  @override
+  State<StatefulWidget> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.text,
+          style: TextStyle(fontSize: _textSize),
         ),
-        body: const Center(
-          child: Text('Hello, world!'),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _textSize += 16.0;
+            });
+          },
+          child: const Text("Perbesar"),
+        )
+      ],
+    );
+  }
+}
+
+// Contoh Scaffold
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Screen"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white
+            ),
+            onPressed: () {}
+          )
+        ],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white
+          ),
+          onPressed: () {}
         ),
+      ),
+      body: const Center(
+        child: Text("Hello World!"),
+      ),
+      // Floating Action Button (FAB)
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
