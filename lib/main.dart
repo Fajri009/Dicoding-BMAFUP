3cb716a5c62eb4d1d1bb1dda992ccaa4268f0ad7
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-07-18 10:51:03 +0700

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const CompareExpandedFlexibleWidget()
+        home: const Navigation()
     );
   }
 }
@@ -739,6 +739,62 @@ class FlexibleWidget extends StatelessWidget {
               fontSize: 24,
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+// Contoh Navigation
+class Navigation extends StatelessWidget {
+  const Navigation({super.key});
+  final String message = "Hello from First Screen!";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Pindah Screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return Navigation2(message);
+              })
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Navigation2 extends StatelessWidget {
+  final String message;
+  const Navigation2(this.message, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(message),
+            OutlinedButton(
+              child: const Text('Kembali'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
         ),
       ),
     );
